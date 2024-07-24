420f3e2cf86fcd26cb643c562e611fcaa138bbd2
billvo <billvo5016@gmail.com>
2024-07-24 13:40:08 -0700

    Add color to the next target to hit

--- Assets/Script/InputHandlerScript.cs
+++ Assets/Script/InputHandlerScript.cs
@@ -8,8 +8,22 @@ public class InputHandlerScript : MonoBehaviour
 {
     private Camera _mainCamera;
 
+    // The array of the ORDERED target names 
+    private string[] arrCircle= new string[] {"Circle5","Circle1","Circle3","Circle8","Circle7","Circle4","Circle2","Circle6","Circle"};
+    private int index = 0;
+
     private void Awake() {
         _mainCamera = Camera.main;
+    }
+
+// Change the color of the first target
+    private void Start() {
+        ChangeColor(index);   //Debug.Log(arrCircle.Length);
+    }
+
+// Change the color of the next target
+    private void Update() {
+        ChangeColor(index);
     }
 
     public void OnClick(InputAction.CallbackContext context)
@@ -19,6 +33,18 @@ public class InputHandlerScript : MonoBehaviour
         var rayHit = Physics2D.GetRayIntersection(_mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
         if(!rayHit.collider) return;
 
-        Debug.Log(rayHit.collider.gameObject.name);
+       
+        if(rayHit.collider.gameObject.name == arrCircle[index]){
+            Debug.Log(arrCircle[index]);
+            index++;
+        }
+       
+    }
+    
+    private void ChangeColor(int index){
+        //string tempCircle;
+        if(index < arrCircle.Length){
+            GameObject.Find(arrCircle[index]).GetComponent<SpriteRenderer>().color = Color.blue;
+        }
     }
 }
